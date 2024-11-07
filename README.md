045ce005d650f4c806c3e610f3d8a5bdb007b751
Eyup <3614029+eyupcol@users.noreply.github.com>
2024-11-07 13:19:49 +0300

    cleanup

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
 # Python Algoritihms Collection
 
 
-You can suggest new algorithm categories or make a pull request to the repository
 
--- common/test_find_max.py
+++
@@ -1,9 +0,0 @@
-from find_max import find_max
-
-
-def test_find_max():
-    """
-    Checks if the number real max number in a list
-    """
-    sample_list = [2, 0, 30, 55, 60, -66]
-    assert find_max(sample_list) == 60
